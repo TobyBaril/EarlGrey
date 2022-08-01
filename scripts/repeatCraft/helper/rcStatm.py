ea13e5cc7c4c949a10ed2688f1ff40c688951023
Toby <46785187+TobyBaril@users.noreply.github.com>
2022-08-01 12:12:17 +0100

    Update rcStatm.py

--- scripts/repeatCraft/helper/rcStatm.py
+++ scripts/repeatCraft/helper/rcStatm.py
@@ -36,21 +36,20 @@ def rcstat(rclabelp,rmergep,outfile,ltrgroup=True):
 				cnt -= 1
 				break
 		
-# delete shitty file headers
-with open(rlabel, "r") as f:
-    lines = f.readlines()
-with open(rlabel, "w") as f:
-    for line in lines:
-        if line.strip("\n") != "##gff-version 3":
-            f.write(line)
+	# delete shitty file headers
+	with open(rlabel, "r") as f:
+		lines = f.readlines()
+	with open(rlabel, "w") as f:
+		for line in lines:
+			if line.strip("\n") != "##gff-version 3":
+				f.write(line)
 
-with open(rmerge, "r") as f:
-    lines = f.readlines()
-with open(rmerge, "w") as f:
-    for line in lines:
-        if line.strip("\n") != "##gff-version 3":
-            f.write(line)
-
+	with open(rmerge, "r") as f:
+		lines = f.readlines()
+	with open(rmerge, "w") as f:
+		for line in lines:
+			if line.strip("\n") != "##gff-version 3":
+				f.write(line)
 
 	with open(rlabel,"r") as f:
 		for i in range(cnt):
@@ -67,26 +66,26 @@ with open(rmerge, "w") as f:
 				teflag = True
 				telabel += 1
 
-				if teD.get(col[2]):
-					teD[col[2]] += 1
-				else:
-					teD[col[2]] = 1
+			if teD.get(col[2]):
+				teD[col[2]] += 1
+			else:
+				teD[col[2]] = 1
 
 			if re.search("LTRgroup=",col[8]):
 				ltrflag = True
 				ltrlabel += 1
 
-				if ltrD.get(col[2]):
-					ltrD[col[2]] += 1
-				else:
-					ltrD[col[2]] = 1
+			if ltrD.get(col[2]):
+				ltrD[col[2]] += 1
+			else:
+				ltrD[col[2]] = 1
 
 			if teflag and ltrflag:
 				teltrlabel += 1
-				if teltrD.get(col[2]):
-					teltrD[col[2]] += 1
-				else:
-					teltrD[col[2]] = 1
+			if teltrD.get(col[2]):
+				teltrD[col[2]] += 1
+			else:
+				teltrD[col[2]] = 1
 
 			teflag = False
 			ltrflag = False
@@ -106,20 +105,20 @@ with open(rmerge, "w") as f:
 			next(f)
 		for line in f:
 			col = line.rstrip().split("\t")
-
+	
 			if rowMerge.get(col[2]):
 				rowMerge[col[2]] += 1
 			else:
 				rowMerge[col[2]] = 1
 
-print("#1. Number of repeats (by class) before and after merge")
-print("=============================================================")
-print(*["repeat class","no. before merge","no. after merge"], sep="\t")
-for c in list(rowRaw.keys()):
-  print(*[c,rowRaw[c],rowMerge[c]],sep="\t")
-print("\n")
-print("#2. Number of repeats (by class) merged by TEgroup and LTRgroup")
-print("=============================================================")
+	print("#1. Number of repeats (by class) before and after merge")
+	print("=============================================================")
+	print(*["repeat class","no. before merge","no. after merge"], sep="\t")
+	for c in list(rowRaw.keys()):
+		print(*[c,rowRaw[c],rowMerge[c]],sep="\t")
+	print("\n")
+	print("#2. Number of repeats (by class) merged by TEgroup and LTRgroup")
+	print("=============================================================")
 	for c in list(teD.keys()):
 		if ltrgroup:
 			if re.search("LTR",c):
@@ -134,8 +133,4 @@ print("=============================================================")
 
 	sys.stdout.close()
 	sys.stdout = stdout
-
-
-
-
 
