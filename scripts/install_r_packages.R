b0162ce3e2af33df3b4f3cb1d005a765588cf928
Toby <46785187+TobyBaril@users.noreply.github.com>
2023-02-24 14:16:30 +0100

    Update install_r_packages.R

--- scripts/install_r_packages.R
+++ scripts/install_r_packages.R
@@ -1,4 +1,4 @@
 # Install R Packages
 
 install.packages(c("BiocManager","optparse", "ape"), repos="https://www.stats.bris.ac.uk/R/")
-BiocManager::install(c("plyranges", "BSgenome"), update = TRUE, ask = FALSE)
+BiocManager::install(c("plyranges", "BSgenome"), update = FALSE, ask = FALSE)
