7339d83f64003481ec9a268b6dd2f2a843e617b2
Tobias Baril <46785187+TobyBaril@users.noreply.github.com>
2024-03-26 11:20:17 +0100

    Update simple_repeat_filter_trim.R

--- scripts/TEstrainer/scripts/simple_repeat_filter_trim.R
+++ scripts/TEstrainer/scripts/simple_repeat_filter_trim.R
@@ -1,5 +1,3 @@
-#!/usr/bin/Rscript
-
 library(optparse)
 
 option_list <- list(
@@ -144,7 +142,7 @@ stats_tr <- full_join(sassr_calc, full_join(mreps_calc, trf_calc, by = "seqnames
 
 # if no data write to og seq to file as nonsatellite and exit
 if(nrow(compiled_tr) == 0){
-  file.create(paste0(opt$directory, "/trf/", opt$out_seq, ".nonsatellite"))
+  writeXStringSet(c(trimmed_seq, untouched_seq), paste0(opt$directory, "/trf/", opt$out_seq, ".nonsatellite"))
   stop()
 }
 
